0b5acce454a2fa8f9c6b6f494596b1eaa38f39d8
PenTestical <57206134+PenTestical@users.noreply.github.com>
2021-06-16 11:06:21 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -9,7 +9,7 @@ sudo mv ../penadventures /var/www/html/penadventures
 # Apache2 Webserver starten und Dateirechte anpassen
 # Ggf. ist Apache2 zu installieren
 sudo service apache2 restart
-sudo chmod 700 -R /var/www/html/penadventures/img/gallery
+sudo chmod 755 -R /var/www/html/penadventures
 
 echo "[+] Script ist fertig, Web-Server ist zu finden unter :"
 echo "http://localhost/penadventures"
